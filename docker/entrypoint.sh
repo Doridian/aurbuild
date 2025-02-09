5bfb384eec90c8ccddb896017d8af45a4506a50e
Doridian <git@doridian.net>
2025-02-08 16:07:16 -0800

    fix sudo

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+chown root:root /etc/sudo.conf /usr/sbin/sudo
+chmod +s /usr/sbin/sudo
+
 usermod -u "${PUID}" aur
 groupmod -g "${PGID}" aur
 mkdir -p /home/aur/.gnupg
