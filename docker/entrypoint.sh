b345d3ae687159811cee2a2589386c9cbe7794c7
Doridian <git@doridian.net>
2025-04-16 15:13:00 -0700

    We should NOT accept private keys via env

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+set -x
 
 ln -sf /dev/stderr /dev/log
 ln -sf /dev/stderr /dev/console
@@ -12,14 +13,6 @@ chown aur:aur /aur
 chmod 700 /home/aur /home/aur/.gnupg
 
 rm -fv /var/lib/pacman/db.lck
-
-if [ ! -z "${GPG_KEY_DATA-}" ]; then
-    if [ -z "${GPG_KEY_ID-}" ]; then
-        echo 'GPG_KEY_ID is not set, but GPG_KEY_DATA is set. Please set GPG_KEY_ID to the key ID of the key.'
-        exit 1
-    fi
-    sudo -H -u aur gpg --no-tty --batch --allow-secret-key-import --yes --import - <<<"${GPG_KEY_DATA}"
-fi
 
 if [ ! -z "${GPG_KEY_PATH-}" ]; then
     if [ -z "${GPG_KEY_ID-}" ]; then
