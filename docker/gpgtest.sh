6ae1abff5bab36b03c717716e4e81c0a85ab6b9f
Doridian <git@doridian.net>
2025-04-16 23:03:42 -0700

    fix

--- docker/gpgtest.sh
+++ docker/gpgtest.sh
@@ -7,6 +7,7 @@ if [ -f /gpg/pin ]; then
     gpg --use-agent --pinentry-mode loopback --passphrase-file /gpg/pin --yes --detach-sign -u "${GPG_KEY_ID}" --output /dev/null /aur/packages.txt
 elif [ -f /gpg/key ]; then
     # Fixed key file
+    true
 else
     echo 'WARNING: Package signing disabled!'
     exit 0
