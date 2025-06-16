bbb53f066114ece60346e4b01bf8e58bc1ea80c1
Doridian <git@doridian.net>
2025-06-15 18:45:26 -0700

    order

--- docker/repo-diff.sh
+++ docker/repo-diff.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -f /tmp/inrepo /tmp/incache
+rm -f /tmp/incache /tmp/inrepo
 
-find /aur/repo -iname '*.pkg.tar*' -printf '%f\n' | sort > /tmp/inrepo
 find /aur/cache -iname '*.pkg.tar*' -printf '%f\n' | sort > /tmp/incache
+find /aur/repo -iname '*.pkg.tar*' -printf '%f\n' | sort > /tmp/inrepo
 
 echo '> means only in repo'
 echo '< means only in cache'
@@ -10,4 +10,4 @@ echo '=============== START ==============='
 diff /tmp/incache /tmp/inrepo
 echo '================ END ================'
 
-rm -f /tmp/inrepo /tmp/incache
+rm -f /tmp/incache /tmp/inrepo
